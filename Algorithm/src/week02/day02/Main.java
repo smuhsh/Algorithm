0abc845dd3a5076387d24ad398dfbb2de2b7f8e0
user <user@DESKTOP-C5EEQRQ>
2024-06-26 09:48:04 +0900

    2024-06-26 09:47 Search greatest common divisor

---
+++ Algorithm/src/week02/day02/Main.java
@@ -0,0 +1,33 @@
+package week02.day02;
+
+import java.util.Scanner;
+
+/*
+ * 작성일:2024-06-26
+ * 작성자:황석현
+ * 개요: 알고리즘 100제 6번 문항
+ * 		입력된 두 수의 최대 공약수를 구하시오
+ * 		ex) 12 18
+ * 		정답 6
+ * 		Tag 반복문(for, while), if
+ * 
+ * 		나는 입력한 숫자 개수만큼 구해보려함
+ * 
+ * */
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("숫자개수를 입력하고 그 수의 최대공약수 구하기 :");
+		Scanner sc = new Scanner(System.in);
+		int input = sc.nextInt();
+		
+		for(int i=0; i<input; i++) {
+			Scanner sc1 = new Scanner(System.in);
+			int input1 = sc1.nextInt();
+		}
+		sc.close();
+		
+	}
+
+}
